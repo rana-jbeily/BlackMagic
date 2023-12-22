cf16f728a7dc5caf6cab0e4fa87943d5fc8582ea
Rana Jbeily <jbeilyrana@gmail.com>
2023-12-22 14:37:47 +0200

    Wordpress Theme

---
+++ blackmagic/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Twenty Twenty-Four functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Twenty Twenty-Four
+ * @since Twenty Twenty-Four 1.0
+ */
