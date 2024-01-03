41101371738a7a2e9f4222bb5dba4d913f78668f
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-01-03 13:26:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 // There is nothing output here because block themes do not use php templates.
 // There is a core ticket discussing removing this requirement for block themes:
 // https://core.trac.wordpress.org/ticket/54272.
+echo "this is the index.phjp page"
